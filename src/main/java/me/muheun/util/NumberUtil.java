2ae3316cd1ee1d5c3682f0b81f01c10ffe2689d9
muheun <muheun.kr@gmail.com>
2023-04-06 00:00:21 +0900

    toLong

--- src/main/java/me/muheun/util/NumberUtil.java
+++ src/main/java/me/muheun/util/NumberUtil.java
@@ -5,32 +5,31 @@ import org.apache.commons.lang3.math.NumberUtils;
 
 public class NumberUtil extends NumberUtils {
 
-  public final static int toIntWithMark(String s) {
-    if (s.startsWith("+")) {
-      return toIntWithMark(s.substring(1));
-    } else {
-      return toInt(s);
+    public final static int toIntWithMark(String s) {
+        if (s.startsWith("+")) {
+            return toIntWithMark(s.substring(1));
+        } else {
+            return toInt(s);
+        }
     }
-  }
 
-  public final static int toInt(Object obj) {
-    return toInt(obj, 0);
-  }
+    public final static int toInt(Object obj) {
+        return toInt(obj, 0);
+    }
 
-  public final static int toInt(Object obj, int dft) {
-    String str = StringUtil.toString(obj, StringUtil.toString(dft));
-    return toInt(str);
-  }
-  public final static long toLong(Object obj) {
-    return toLong(obj, 0);
-  }
+    public final static int toInt(Object obj, int dft) {
+        return toInt(StringUtil.toString(obj), dft);
+    }
 
-  public final static long toLong(Object obj, int dft) {
-    String str = StringUtil.toString(obj, StringUtil.toString(dft));
-    return toLong(str);
-  }
+    public final static long toLong(Object obj) {
+        return toLong(obj, 0L);
+    }
 
-  public final static boolean isModify(long l, int mod) {
-    return l % mod == 0;
-  }
+    public final static long toLong(Object obj, long dft) {
+        return toLong(StringUtil.toString(obj), dft);
+    }
+
+    public final static boolean isModify(long l, int mod) {
+        return l % mod == 0;
+    }
 }
